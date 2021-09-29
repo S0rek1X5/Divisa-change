747aad8b1f4ec65be2b539aab1d79ce744d2b211
Daniel <danielbebon@gmail.com>
2021-09-29 13:13:51 -0500

    Update Changer.c

--- Changer.c
+++ Changer.c
@@ -14,49 +14,41 @@ int main ()
 		scanf("%d", &divisa);
 		if (divisa==1){
 			printf("\nSu cantidad es-> %1.2f", bolivar,"Dolares");
-			printf("\n----------------------------------");
 		}
 		else if(divisa==2){
 			printf("\nSu cantidad es-> %1.2f", canadiense,"Dolares");
-			printf("\n----------------------------------");
 		}
 		else if(divisa==3){
 			printf("\nSu cantidad es-> %1.2f", libra,"Dolares");
-			printf("\n----------------------------------");
 		}
 		else if(divisa==4){
 			printf("\nSu cantidad es-> %1.2f", pesoCol,"Dolares");
-			printf("\n----------------------------------");
 		}
 		else if(divisa==5){
 			printf("\nSu cantidad es-> %1.2f", pesoMex,"Dolares");
-			printf("\n----------------------------------");
 		}
 		else if(divisa==6){
 			printf("\nSu cantidad es-> %1.2f", pesoArg,"Dolares");
-			printf("\n----------------------------------");
 		}
 		else if(divisa==7){
 			printf("\nSu cantidad es-> %1.2f", yen,"Dolares");
-			printf("\n----------------------------------");
 		}
 		else if(divisa==8){
 			printf("\nSu cantidad es-> %1.2f", yuan,"Dolares");
-			printf("\n----------------------------------");
+
 		}
 		else if(divisa==9){
 			printf("\nSu cantidad es-> %1.2f", euro,"Dolares");
-			printf("\n----------------------------------");
 		}
 		else if(divisa==10){
 			printf("\nSu cantidad es-> %1.2f", realbr,"Dolares");
-			printf("\n----------------------------------");
 		}
 		else{
 			printf("\nERROR SELECCIONE CORRECTAMENTE EL TIPO DE MONEDA QUE INGRESO\n");
-			printf("\n----------------------------------");
 		}
 		printf("\nPresione 1 para CONTINUAR --- 2 para SALIR\n: ");scanf("%d",&opc);
+		printf("\n----------------------------------");
 	}while(opc==1);
 	return 0;
+}
 }
